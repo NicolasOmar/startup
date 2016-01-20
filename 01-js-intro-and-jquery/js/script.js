0161f411746a0f34ee6ca907c3a1586751ff7eba
NicolasOmar <nicolas.passerino@gmail.com>
2016-01-20 19:46:12 -0300

    Subida de Topico 1. Punto 7 resuelto parcialmente (falta hacer la conexion, se agregan ajax de prueba)

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -9,22 +9,41 @@ $(document).ready(function () {
 	Nicolas?callback=jQuery112006777946413494647_1453313942252&_=1453313942253:1 Uncaught SyntaxError: Unexpected token :
 	*/
   $("#point-7").click(function () {	
-    $.ajax({
-		type:"get",
+  
+	var text = "Hello <div id='highName'>Nicolas</div>, Websites regularly witnesses many new users and people who register for their account online in these websites. As such, the websites send welcome messages to the users once they make their registration and their account get activated. Wednesday the 20th";
+	$(".hidden > h2").empty();
+	$(".hidden > h2").append(text);
+	$("#highName").css({
+					"background-color":"blue",
+					"color":"white",
+					"display":"inline"});
+	/*$.ajax({
+		url : "http://bootcamp.dsupport2000.com/Nicolas",
+		dataType:"jsonp",
+		jsonp:"response",
+		success: function( response )
+		{
+			console.log( response ); 
+		},
+	});*/
+
+	/*$.ajax({
 		url: "http://bootcamp.dsupport2000.com/Nicolas",
-	 
-		contentType: 'application/json;charset=utf-8',
-		dataType: "jsonp",
-		crossDomain:true,
-		jsonCallback: 'json_callback',
-		
-		success: function( response ) { console.log( response ); },
-		error: function(){
-			console.log( "Error, Try again with other idea" )
-			$(".hidden").css("background-color","red")
-			$(".hidden > h2").css("color","white")
-			}
-	});
+		type: "GET",
+		data: null,
+		contentType: "application/json",
+		dataType: "json",
+		headers:
+		{
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "*",
+			"Access-Control-Allow-Methods": "*"
+		},
+		success: function (result)
+		{
+			console.log("Success!!! " + result);
+		}
+	});	*/
   });
   
   $("#btn-spotify1").click(function() {
