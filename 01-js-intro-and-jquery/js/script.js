a4515c87c246e54cc4385500a2d956515aa97be1
NicolasOmar <nicolas.passerino@gmail.com>
2016-01-21 11:15:02 -0300

    Topico 1 terminado, puntos faltantes resueltos

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -3,47 +3,34 @@ $(document).ready(function () {
 	$(".hidden").fadeIn(1500);
 	$(".alias").focus();
   });	
-    
-/*
-	i'm having problems with point 7 for now, the console throw me this error:
-	Nicolas?callback=jQuery112006777946413494647_1453313942252&_=1453313942253:1 Uncaught SyntaxError: Unexpected token :
-	*/
-  $("#point-7").click(function () {	
   
-	var text = "Hello <div id='highName'>Nicolas</div>, Websites regularly witnesses many new users and people who register for their account online in these websites. As such, the websites send welcome messages to the users once they make their registration and their account get activated. Wednesday the 20th";
-	$(".hidden > h2").empty();
-	$(".hidden > h2").append(text);
-	$("#highName").css({
-					"background-color":"blue",
-					"color":"white",
-					"display":"inline"});
-	/*$.ajax({
-		url : "http://bootcamp.dsupport2000.com/Nicolas",
-		dataType:"jsonp",
-		jsonp:"response",
-		success: function( response )
-		{
-			console.log( response ); 
-		},
-	});*/
-
-	/*$.ajax({
-		url: "http://bootcamp.dsupport2000.com/Nicolas",
-		type: "GET",
-		data: null,
-		contentType: "application/json",
-		dataType: "json",
-		headers:
-		{
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Headers": "*",
-			"Access-Control-Allow-Methods": "*"
-		},
-		success: function (result)
-		{
-			console.log("Success!!! " + result);
-		}
-	});	*/
+  $("#point-7").click(function () {	
+	$.ajax({
+	  type: 'GET',
+	  url: 'http://bootcamp.dsupport2000.com/Nicolas',
+	  contentType: 'text/plain',
+	  xhrFields: {
+		withCredentials: false
+	  },
+	  success: function(request) {
+		var greetingArray = request.response.greeting.split(" ");
+		var text =  greetingArray[0] + " <div id='highName'>" + greetingArray[1] + "</div>. " + request.response.Welcome + ". " + request.response.Date  + ".";
+		$(".hidden > h2").empty();
+		$(".hidden > h2").append(text);
+		$("#highName").css({
+						"background-color":"blue",
+						"color":"white",
+						"display":"inline"});
+	  },
+	  error: function() {
+		$(".hidden > h2").empty();
+		$(".hidden > h2").text("ERROR");
+		$("#highName").css({
+						"background-color":"red",
+						"color":"white",
+						"display":"inline"});
+	  }
+	});
   });
   
   $("#btn-spotify1").click(function() {
