bd8b8bce26652a4057bd4af971dc2e229ab11d53
NicolasOmar <nicolas.passerino@gmail.com>
2016-01-25 07:55:52 -0300

    Topico 1 terminado. Revision final de codigo

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -1,51 +1,47 @@
 $(document).ready(function () {
   $( window ).load(function() {
+	//5 - When the page has finished loading the section must fade in.	
 	$(".hidden").fadeIn(1500);
+	//6 - Add a textbox with the class "alias", and put the cursor inside it right after the section fades in.
+	$(".first").append("<input type='text' class='alias' />")
 	$(".alias").focus();
   });	
-    
-/*
-	i'm having problems with point 7 for now, the console throw me this error:
-	Nicolas?callback=jQuery112006777946413494647_1453313942252&_=1453313942253:1 Uncaught SyntaxError: Unexpected token :
-	*/
+  
   $("#point-7").click(function () {	
   
-	var text = "Hello <div id='highName'>Nicolas</div>, Websites regularly witnesses many new users and people who register for their account online in these websites. As such, the websites send welcome messages to the users once they make their registration and their account get activated. Wednesday the 20th";
-	$(".hidden > h2").empty();
-	$(".hidden > h2").append(text);
-	$("#highName").css({
-					"background-color":"blue",
-					"color":"white",
-					"display":"inline"});
-	/*$.ajax({
-		url : "http://bootcamp.dsupport2000.com/Nicolas",
-		dataType:"jsonp",
-		jsonp:"response",
-		success: function( response )
-		{
-			console.log( response ); 
-		},
-	});*/
-
-	/*$.ajax({
-		url: "http://bootcamp.dsupport2000.com/Nicolas",
-		type: "GET",
-		data: null,
-		contentType: "application/json",
-		dataType: "json",
-		headers:
-		{
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Headers": "*",
-			"Access-Control-Allow-Methods": "*"
-		},
-		success: function (result)
-		{
-			console.log("Success!!! " + result);
-		}
-	});	*/
+	//7 - Attach an event to the created button which calls a function that gets a response from http://bootcamp.dsupport2000.com/Nicolas
+	$.ajax({
+	  type: 'GET',
+	  url: 'http://bootcamp.dsupport2000.com/Nicolas',
+	  contentType: 'text/plain',
+	  xhrFields: {
+		withCredentials: false
+	  },
+	  //8 - Write the response to the section element.
+	  success: function(request) {
+		//10 - Take some free air and then create a function to highlight your name in the server response content. Call it right after setting the response inside the div.
+		var greetingArray = request.response.greeting.split(" ");
+		var text =  greetingArray[0] + " <div id='highName'>" + greetingArray[1] + "</div>. " + request.response.Welcome + ". " + request.response.Date  + ".";
+		$(".hidden > h2").empty();
+		$(".hidden > h2").append(text);
+		$("#highName").css({
+						"background-color":"blue",
+						"color":"white",
+						"display":"inline"});
+	  },
+	  //9 - Show section content in red when a server error occurs.
+	  error: function() {
+		$(".hidden > h2").empty();
+		$(".hidden > h2").text("ERROR");
+		$(".hidden > h2").css({
+						"background-color":"red",
+						"color":"white",
+						"display":"inline"});
+	  }
+	});
   });
   
+  //11 - AJAX: get the response from https://api.spotify.com/v1/search with parameters data "q = 'Rolling Stones', type = 'album'" First log the service response in Chrome's console to analyze data (see provided link on Chrome console), then display albums inside another section in the right side of the screen. The article element must be used to contain the album data. For each album show: name, type, image, release_date, and a link to spotify for that album.
   $("#btn-spotify1").click(function() {
 	  $("#art-spotify").empty();
 	  $.ajax(
@@ -68,6 +64,7 @@ $(document).ready(function () {
       });
     });
   
+  //12 - Add an input type="text", and reuse the code for exercise 11, so the user can perform search for any artist albums.
   $("#btn-spotify2").click(function() {
     var artist = $("#txt-spotify").val();
 	if(artist != "")
@@ -100,6 +97,5 @@ $(document).ready(function () {
 	{
 	  alert("Please, write the artist name to search its albums in spotify");
       $("#txt-spotify").focus(); }
-  });
-  
+  });  
 });
