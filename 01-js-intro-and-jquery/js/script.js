9f59bc1da2e5158f7344c9893e26d761fc23d245
NicolasOmar <nicolas.passerino@gmail.com>
2016-01-31 00:18:54 -0300

    Topic 1 finished. Code errors fixed

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -1,96 +1,5 @@
 $(document).ready(function () {
   $( window ).load(function() {
-<<<<<<< Updated upstream
-	$(".hidden").fadeIn(1500);
-	$(".alias").focus();
-  });	
-  
-  $("#point-7").click(function () {	
-	$.ajax({
-	  type: 'GET',
-	  url: 'http://bootcamp.dsupport2000.com/Nicolas',
-	  contentType: 'text/plain',
-	  xhrFields: {
-		withCredentials: false
-	  },
-	  success: function(request) {
-		var greetingArray = request.response.greeting.split(" ");
-		var text =  greetingArray[0] + " <div id='highName'>" + greetingArray[1] + "</div>. " + request.response.Welcome + ". " + request.response.Date  + ".";
-		$(".hidden > h2").empty();
-		$(".hidden > h2").append(text);
-		$("#highName").css({
-						"background-color":"blue",
-						"color":"white",
-						"display":"inline"});
-	  },
-	  error: function() {
-		$(".hidden > h2").empty();
-		$(".hidden > h2").text("ERROR");
-		$("#highName").css({
-						"background-color":"red",
-						"color":"white",
-						"display":"inline"});
-	  }
-	});
-  });
-  
-  $("#btn-spotify1").click(function() {
-	  $("#art-spotify").empty();
-	  $.ajax(
-      {
-  	    url: 'https://api.spotify.com/v1/search',
-	    type: 'GET',
-	    data: { "q": 'Rolling Stones', "type": 'album' },
-	    dataType: 'json',
-	    success: function (respuesta) 
-	    {
-	      $.each(respuesta, function(index, element)
-	      {
-		    $.each(element.items, function(index, item)
-		    {
-		  	  var album = '<div class="album"> Name: ' + item.name + "<br>Type: " + item.type + "<br>Album Cover<br><img src='" + item.images[0].url + "' /><br><a href='" + item.external_urls.spotify + "'>Listen it in Spotify</href></div>";
-			  $("#art-spotify").append(album)
-		    })
-	      })
-	    }
-      });
-    });
-  
-  $("#btn-spotify2").click(function() {
-    var artist = $("#txt-spotify").val();
-	if(artist != "")
-	{
-	  $("#txt-spotify").val("");
-	  $("#art-spotify").empty();
-	  $.ajax(
-	  {
-		url: 'https://api.spotify.com/v1/search',
-	    type: 'get',
-	    data: { "q": artist, "type": 'album' },
-	    dataType: 'json',
-	    success: function (respuesta) 
-		{
-		  $("#art-spotify").text("");
-		  $.each(respuesta, function(index, element)
-		  {
-			$.each(element.items, function(index, item)
-			{
-			  console.log(item);
-			  var album = '<div class="album"> Name: ' + item.name + "<br>Type: " + item.type + "<br>Album Cover<br><img src='" + item.images[0].url + "' /><br><a href='" + item.external_urls.spotify + "'>Listen it in Spotify</href></div>";
-			  $("#art-spotify").append(album);
-			})
-		  })
-	    }
-	  });
-	  $("#txt-spotify").focus();
-	}
-	else
-	{
-	  alert("Please, write the artist name to search its albums in spotify");
-      $("#txt-spotify").focus(); }
-  });
-  
-=======
     //5 - When the page has finished loading the section must fade in.  
     $('.hidden-content').fadeIn(1500);
     //6 - Add a textbox with the class 'alias', and put the cursor inside it right after the section fades in.
@@ -186,6 +95,5 @@ $(document).ready(function () {
       alert('Please, write the artist name to search its albums in spotify');
       $('#txt-spotify').focus();
 	}
-  });  
->>>>>>> Stashed changes
+  });
 });
