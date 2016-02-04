283a7da8317478a4ade6bb8c3c4a8fa5e8cac040
NicolasOmar <nicolas.passerino@gmail.com>
2016-02-04 14:05:15 -0300

    Topic 3 uptade. Code error (require) solved with browserify

--- 03-Node-npm-modules/js/director.js
+++ 03-Node-npm-modules/js/director.js
@@ -1,3 +1,4 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Director = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function Director(Name) {
 //6 - Add name:string, a quotes:array properties, and a speak() method to Director; calling speak() will return director’s quotes.
   this.name = Name;
@@ -43,3 +44,5 @@ function Director(Name) {
 }
 //4 - Create a Director class inside a module and set it as a dependency on the Movie module. Tip: use require.
 module.exports = Director;
+},{}]},{},[1])(1)
+});
--- 03-Node-npm-modules/js/movies.js
+++ 03-Node-npm-modules/js/movies.js
@@ -1,6 +1,7 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Movie = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function Director(Name) {
-//6 - Add name:string, a quotes:array properties, and a speak() method to Director; calling speak() will return director’s quotes.
+//6 - Create a movies.js file using browserify. Add that script to an  index.html. Check that it works opening it in the browser.
+//NOTE: File created using browserify movie.js --standalone Movie > movies.js
   this.name = Name;
   this.quotes = [];
 	
@@ -102,5 +103,5 @@ Movie.prototype = {
 }
 //3 - Create the same Movie class as in the previous practice, but inside a CommonJS module. Tip: use module.exports.
 module.exports = Movie;
-
-},{"./director":1}]},{},[2]);
+},{"./director":1}]},{},[2])(2)
+});
